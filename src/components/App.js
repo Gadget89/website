1b22141de425bc1cc1df35ea2366095147aa9902
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-11-05 11:45:55 -0600

    Initalized template for website

--- src/components/App.js
+++ src/components/App.js
@@ -7,23 +7,24 @@ import {
 
 // App components
 import Header from './Header';
-import Landing from './Landing';
+import Home from './Home';
 import About from './About';
-import Skills from './Skills';
-import Projects from './Projects';
+import Teachers from './Teachers';
+import Courses from './Courses';
 import NotFound from './NotFound';
+import Featured from './Featured';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
-
+      
       <Switch>
-        <Route exact path="/" component={Landing} />
+        <Route exact path="/" component={Home} />
         <Route path="/about" render={ () => <About title='About' /> } />
-        <Route exact path="/projects" component={Projects} />
-        <Route path="/teachers/:topic/:name" component={Projects} />
-        <Route path="/skills" component={Skills} />
+        <Route exact path="/teachers" component={Teachers} />
+        <Route path="/teachers/:topic/:name" component={Featured} />
+        <Route path="/courses" component={Courses} />
         <Route component={NotFound} />
       </Switch>
     </div>
@@ -31,3 +32,7 @@ const App = () => (
 );
 
 export default App;
+
+
+
+
