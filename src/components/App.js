7d67cdd9d90615347ff836c3d848b77a1cfe58e5
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-11-04 23:57:52 -0500

    Incorperated react router functionality to be routed through App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
-}
+// App components
+import Header from './Header';
+import Landing from './Landing';
+import About from './About';
+import Skills from './Skills';
+import Projects from './Projects';
+import NotFound from './NotFound';
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/about" render={ () => <About title='About' /> } />
+        <Route exact path="/projects" component={Projects} />
+        <Route path="/teachers/:topic/:name" component={Projects} />
+        <Route path="/skills" component={Skills} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
