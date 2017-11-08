334e6a04bda43557710de4942cdc80d31225fdce
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-11-08 10:43:31 -0600

    Added final styling for all screen sizes to render header navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import Logo from '../assets/logo.png';
+import Logo from '../assets/dark-logo.png';
+// import LightLogo from '../assets/light-logo.png';
 
+
+
+const hdrRibbon1 = () => (
+  document.getElementById('hdrWrapper').className = "ribbon-syl-1"
+)
+const hdrRibbon2 = () => (
+  document.getElementById('hdrWrapper').className = "ribbon-syl-2"
+)
+const hdrRibbon3 = () => (
+  document.getElementById('hdrWrapper').className = "ribbon-syl-3"
+)
+const hdrRibbon4 = () => (
+  document.getElementById('hdrWrapper').className = "ribbon-syl-4"
+)
 const Header = () => (
-  <header>
-    <img className="icn-logo" src={Logo} alt="" />
+
+  <header id="hdrWrapper">
+    <span className="navImgContainer">
+      <img className="icn-logo" src={Logo} alt="" />
+    </span>
     <ul className="main-nav">
-      <li><NavLink exact to="/">Home</NavLink></li>
-      <li><NavLink to="/about">About</NavLink></li>
-      <li><NavLink to="/teachers">Projects</NavLink></li>
-      <li><NavLink to="/contact">Contact Me</NavLink></li>
+      <li><NavLink onClick={hdrRibbon1} exact to="/">Home</NavLink></li>
+      <li><NavLink onClick={hdrRibbon2} to="/about">About</NavLink></li>
+      <li><NavLink onClick={hdrRibbon3} to="/teachers">Projects</NavLink></li>
+      <li><NavLink onClick={hdrRibbon4} to="/contact">Contact Me</NavLink></li>
     </ul>
   </header>
 );
