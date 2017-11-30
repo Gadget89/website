89b64271fed49d3a43e22639a011fc1131831e73
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-11-29 23:06:53 -0600

    Added content to About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,15 @@ import React from 'react';
 const About = (props) => (
   <div className="main-content">
     <h2>{ props.title }</h2>
-    <p>The front end course directory lists many of the courses we teach on HTML, CSS, JavaScript and more! Be sure to visit the Teachers section to view a list of our talented teachers. Or visit the Courses section and select a topic -- HTML, CSS, or JavaScript -- to see a list of our courses.</p>
+    <p>
+      I began my journey to become a software developer in the Fall of 2016. I've had the great privilege to learn from accomplished instructors at The Iron Yard and I graduated from their Front-End Engineering program in February 2017.
+    </p>
+    <p>
+      After gaining a solid foundation, I am excited for this lifetime adventure. My curiosity and passion for code continue driving me to keep learning as much as I can.
+    </p>
+    <p>
+      I would like to join a great team and be involved in projects that I can be proud of. I invite you to check out my projects and please feel free to contact me.
+    </p>
   </div>
 );
 
