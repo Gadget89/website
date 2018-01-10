a0fed100724ec239fd1c88758f86aa09919599b7
Nicolas Roybal <nicolas.roybal@hotmail.com>
2018-01-09 19:23:42 -0700

    Removed on click handlers for navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,18 @@ import { NavLink } from 'react-router-dom';
 
 import Logo from '../assets/dark-logo.png';
 
-const hdrRibbon1 = () => (
-  document.getElementById('hdrWrapper').className = "ribbon-syl-1"
-)
-const hdrRibbon2 = () => (
-  document.getElementById('hdrWrapper').className = "ribbon-syl-2"
-)
-const hdrRibbon3 = () => (
-  document.getElementById('hdrWrapper').className = "ribbon-syl-3"
-)
-const hdrRibbon4 = () => (
-  document.getElementById('hdrWrapper').className = "ribbon-syl-4"
-)
+// const hdrRibbon1 = () => (
+//   document.getElementById('hdrWrapper').className = "ribbon-syl-1"
+// )
+// const hdrRibbon2 = () => (
+//   document.getElementById('hdrWrapper').className = "ribbon-syl-2"
+// )
+// const hdrRibbon3 = () => (
+//   document.getElementById('hdrWrapper').className = "ribbon-syl-3"
+// )
+// const hdrRibbon4 = () => (
+//   document.getElementById('hdrWrapper').className = "ribbon-syl-4"
+// )
 const Header = () => (
 
   <header id="hdrWrapper">
@@ -22,10 +22,10 @@ const Header = () => (
       <img className="icn-logo" src={Logo} alt="" />
     </span>
     <ul className="main-nav">
-      <li><NavLink onClick={hdrRibbon1} exact to="/">Home</NavLink></li>
-      <li><NavLink onClick={hdrRibbon2} to="/about">About</NavLink></li>
-      <li><NavLink onClick={hdrRibbon3} to="/projects">Projects</NavLink></li>
-      <li><NavLink onClick={hdrRibbon4} to="/contact">Contact Me</NavLink></li>
+      <li><NavLink exact to="/">Home</NavLink></li>
+      <li><NavLink to="/about">About</NavLink></li>
+      <li><NavLink to="/projects">Projects</NavLink></li>
+      <li><NavLink to="/contact">Contact Me</NavLink></li>
     </ul>
   </header>
 );
